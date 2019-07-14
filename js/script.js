7fc4f4c055d0e5d1cb09446a0939af5e51122d85
Klaudia Sławińska <klaudia.slawinska94@gmail.com>
2019-07-14 16:12:12 +0200

    add class active to the clicked link

--- js/script.js
+++ js/script.js
@@ -4,16 +4,19 @@ function titleClickHandler(){
   console.log('Link was clicked!');
   console.log(event);
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for(let activeLink of activeLinks)
   {
     activeLink.classList.remove('active');
   }
-  /* add class 'active' to the clicked link */
+  /* [DONE] add class 'active' to the clicked link */
+  const clickedElement = this;
+  console.log('clickedElement:', clickedElement);
+  clickedElement.classList.add('active');
 
-  /* remove class 'active' from all articles */
+  /* [DONE] remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.posts .active');
 
   for(let activeArticle of activeArticles)
